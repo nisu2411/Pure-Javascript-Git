f724c37cbd6cdc8af12ad4230bfad1c81b91d668
Nisarg Patel <nisargpatel1198@gmail.com>
2023-04-05 02:26:19 +0530

    Changed Logic for start ad offset

--- functions/prepareStartOffset.js
+++ functions/prepareStartOffset.js
@@ -3,13 +3,28 @@ const NetworksList = require("../models/networksList");
 async function prepareStartOffset(query, start, offset, limit) {
   let networks;
   let totalNetworksCount;
+  let queryObject = query ? query : {};
 
-  if (start && offset) {
-    networks = await NetworksList.find(query)
-      .skip(start - 1)
-      .limit(offset);
-  } else {
-    networks = await NetworksList.find(query).limit(limit);
+  switch (true) {
+    case (!start && !offset && Object.keys(queryObject).length === 0):
+      networks = await NetworksList.find().limit(limit);
+      break;
+    case (start && offset && query):
+      networks = await NetworksList.find(queryObject)
+        .skip(start - 1)
+        .limit(offset);
+      break;
+    case ((start <= 0 || offset <= 0) && Object.keys(queryObject).length !== 0):
+    case (!start && !offset && query):
+    case (!start && !offset && query === ''):
+    case (start <= 0 || offset <= 0):
+      networks = await NetworksList.find(queryObject);
+      break;
+    case (!start && !offset && !query):
+    case (start && offset && !query):
+    default:
+      networks = await NetworksList.find().limit(limit);
+      break;
   }
 
   totalNetworksCount = await NetworksList.countDocuments();
