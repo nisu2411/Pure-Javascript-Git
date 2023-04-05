2d073d49e3d8e47e9c41b8f35904707e565ed3b1
Nisarg Patel <nisargpatel1198@gmail.com>
2023-04-05 20:20:18 +0530

    Improved Switch Case Logic

--- functions/prepareStartOffset.js
+++ functions/prepareStartOffset.js
@@ -1,33 +1,36 @@
 const NetworksList = require("../models/networksList");
 
-
 async function prepareStartOffset(query, start, offset, limit) {
   let networks;
   let totalNetworksCount;
-  let queryObject = query ? query : {};
-
+  
   switch (true) {
-    case (!start && !offset && Object.keys(queryObject).length === 0):
+    case (!start && !offset && (Object.keys(query).length === 0)):
       networks = await NetworksList.find().limit(limit);
+      break; 
+    case ((start <= 0 || offset <= 0) && !(Object.keys(query).length === 0)):
+      networks = await NetworksList.find(query);
+      break; 
+    case ((start <= 0 || offset <= 0)):
+      networks = await NetworksList.find().limit(limit);
+      break; 
+    case (!start && !offset && !(Object.keys(query).length === 0)):
+      networks = await NetworksList.find(query);
+      break; 
+    case (start && offset && !(Object.keys(query).length === 0)):
+      networks = await NetworksList.find(query).skip(start - 1).limit(offset);
+      break; 
+    case (!start && !offset && (Object.keys(query).length === 0)):
+      networks = await NetworksList.find().limit(limit);
+      break; 
+    case (start && offset && (Object.keys(query).length === 0)):
+      networks = await NetworksList.find().skip(start - 1).limit(offset);
       break;
-    case (start && offset && query):
-      networks = await NetworksList.find(queryObject)
-        .skip(start - 1)
-        .limit(offset);
-      break;
-    case ((start <= 0 || offset <= 0) && Object.keys(queryObject).length !== 0):
-    case (!start && !offset && query):
-    case (!start && !offset && query === ''):
-    case (start <= 0 || offset <= 0):
-      networks = await NetworksList.find(queryObject);
-      break;
-    case (!start && !offset && !query):
-    case (start && offset && !query):
-      networks = await NetworksList.find(queryObject);
+    case ((!start && !offset && query === '')):
+      networks = await NetworksList.find().limit(limit);
       break;
     default:
       networks = await NetworksList.find().limit(limit);
-      break;
   }
 
   totalNetworksCount = await NetworksList.countDocuments();
