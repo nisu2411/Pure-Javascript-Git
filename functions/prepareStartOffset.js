46748f435b553242bd42303a2523eeefbae2e4d2
Nisarg Patel <nisargpatel1198@gmail.com>
2023-04-07 13:08:04 +0530

    PrepareStartOffset Updated

--- functions/prepareStartOffset.js
+++ functions/prepareStartOffset.js
@@ -11,6 +11,9 @@ async function prepareStartOffset(query, start, offset, limit) {
     case ((start <= 0 || offset <= 0) && !(Object.keys(query).length === 0)):
       networks = await NetworksList.find(query);
       break; 
+    case ((isNaN(start) || isNaN(offset)) && !(Object.keys(query).length === 0)):
+        networks = await NetworksList.find(query);
+        break; 
     case ((start <= 0 || offset <= 0)):
       networks = await NetworksList.find().limit(limit);
       break; 
